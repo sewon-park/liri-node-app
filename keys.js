c9d37322548340a5806584de42b04aab1b48f1de
won518 <sewonpark518@gmail.com>
2019-05-04 12:36:32 -0700

    modifying keys.js to hide api keys

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.c5a872f931b24553b1e43e5e09768669,
-  secret: process.env.f03ea90bcadf4fbeb91056020d5f40cf
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
